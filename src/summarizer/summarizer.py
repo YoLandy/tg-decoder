659ffa40425fe2f79c8440ac0fab86d3992c16f3
FMariaD <fedorova.md@phystech.edu>
2023-11-21 08:47:15 +0300

    better slicing
    better slicing for text, new prompt for GigaChat, postprocessing

--- src/summarizer/summarizer.py
+++ src/summarizer/summarizer.py
@@ -4,14 +4,14 @@ from langchain.schema import HumanMessage, SystemMessage
 from langchain.chat_models.gigachat import GigaChat
 
 MODEL = "gpt-3.5-turbo-1106"
-MAX_SYMBOLS = 15000
+MAX_WORDS = 1000  # max_tokens = 2048
 
 
 system_prompt = '''Твоя задача выделить главную информацию из записи совещания. 
-            Эта информация должна быть удобным сокращением текста, который содержится в тройных кавычках.
             Выдели несколько главных тезисов, которые обсуждались и перечисли их.
-            Постарайся сохранить важные детали.'''
-
+            Постарайся сохранить важные детали.
+            Твой ответ должен содержать ТОЛЬКО ТЕЗИСЫ, в формате "{порядковый_номер}. {Тезис}\n".
+            '''
 class Summarizer():
     def __init__(self, combined_text):
         self.texts = [text for (text, speaker) in combined_text]
@@ -19,17 +19,19 @@ class Summarizer():
         self.chat = GigaChat(credentials=SBER_API_KEY, verify_ssl_certs=False)        
 
     def summarize(self):
-        # change later !!!
         summaries = self.get_summary()
+        size = len(summaries)
         summary = ''
         for i, part in enumerate(summaries):
-            summary += part + '\n'
+            summary += f'Часть {i + 1} из {size}:\n'
+            if part.split('\n')[0] == 'Тезисы:':
+                part = '\n'.join(part.split('\n')[1:])
+            summary += part + '\n\n'
         return summary
 
     def get_summary(self):
         texts = self.prepare_texts()
         result = []
-
         for text in texts:
             prompt = text
             try:
@@ -43,23 +45,20 @@ class Summarizer():
         curr_size = 0
         curr_text = ''
         text = []
-        last_speaker = self.speakers[0]
-
-        for i, line in enumerate(self.texts):
-            line = '\n' + last_speaker + ': ' + line
-            if len(line) + curr_size < MAX_SYMBOLS:
+        for line in self.texts:
+            num_words = len(line.split(' '))
+            if num_words + curr_size < MAX_WORDS:
                 curr_text += line
-                curr_size += len(line)
+                curr_size += num_words
             else:
                 text.append(curr_text)
                 curr_text = line
-                curr_size = len(line)
+                curr_size = num_words
         text.append(curr_text)
         return text
 
     def get_completion(self, prompt, model=MODEL):
         messages = [SystemMessage(content=system_prompt)]
         messages.append(HumanMessage(content=prompt))
-        
         response = self.chat(messages)
         return response.content
